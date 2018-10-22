817690a4cb9cb6b9648cbe0662ada23c3c8d100d
Thomas Gracie <tgracie030@gmail.com>
2018-10-22 09:45:39 +0100

    CSS added extensions complete

--- js/app.js
+++ js/app.js
@@ -12,21 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleFormSubmit = function (event){
   event.preventDefault();
 
+  const listItem = document.createElement('div');
+  listItem.classList.add('list-item');
   const make = document.createElement('h2');
   const model = document.createElement('h3');
   const bodystyle = document.createElement('h4');
-  const bodyStyle = document.createElement('p');
+  const vehicleClass = document.createElement('p');
 
   make.textContent = `${this.make.value}`;
   model.textContent = `${this.model.value}`;
   bodystyle.textContent = `${this.bodystyle.value}`;
-  bodyStyle.textContent = `${this.bodyStyle.value}`;
+  vehicleClass.textContent = `${this.vehicleClass.value}`;
 
   const result = document.querySelector('#vehicle-list');
-  result.appendChild(make);
-  result.appendChild(model);
-  result.appendChild(bodystyle);
-  result.appendChild(bodyStyle);
+  result.appendChild(listItem);
+  listItem.appendChild(make);
+  listItem.appendChild(model);
+  listItem.appendChild(bodystyle);
+  listItem.appendChild(vehicleClass);
   document.getElementById('new-items').reset()
 };
 
